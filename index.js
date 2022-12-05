e37a2b6cd31bc980e12e72722f7827fdeb8400be
skandog <muumrar@gmail.com>
2022-12-05 12:05:30 +0000

    almost there - created function to generate array of letters and function to calc score of letters in a word'

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var puzzleInput = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
 var puzzleInputArr = puzzleInput.split('\n');
 var dupes = [];
-function anythingInCommon(a, b) {
+function charInCommon(a, b) {
     // if( b.length < a.length )
-    //     return anythingInCommon(b, a)
+    //     return charInCommon(b, a)
     for (var i = 0, len = a.length; i < len; i++)
         if (b.indexOf(a[i]) != -1)
             return (a[i]);
     return false;
 }
+var key = (function () {
+    var caps = __spreadArray([], Array(26), true).map(function (val, i) { return String.fromCharCode(i + 65); });
+    var lower = caps.map(function (letter) { return letter.toLowerCase(); });
+    return lower.concat(caps);
+})();
+function wordScore(word) {
+    var score = 0;
+    for (var j = 0; j < word.length; j++) {
+        var char = word[j];
+        score += (key.indexOf(char) + 1);
+    }
+    return score;
+}
+console.log(wordScore('A'));
 for (var i = 0; i < puzzleInputArr.length; i++) {
     var stringLength = puzzleInputArr[i].length;
     var left = puzzleInputArr[i].slice(0, stringLength / 2);
     var right = puzzleInputArr[i].slice(stringLength / 2, stringLength);
     //console.log('left :>> ', left);
     //console.log('right :>> ', right);
-    console.log(anythingInCommon(left, right));
+    console.log(charInCommon(left, right));
 }
 // console.log(puzzleInputArr);
--- index.ts
+++ index.ts
@@ -11,9 +11,9 @@ let puzzleInputArr: any = puzzleInput.split('\n')
 
 let dupes: string[] = [];
 
-function anythingInCommon(a, b){
+function charInCommon(a, b){
     // if( b.length < a.length )
-    //     return anythingInCommon(b, a)
+    //     return charInCommon(b, a)
 
     for( var i = 0, len = a.length; i < len; i++ ) 
         if(b.indexOf(a[i]) != -1)
@@ -22,13 +22,32 @@ function anythingInCommon(a, b){
     return false
 }
 
+
+
+const key = (() => {
+    const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+    const lower = caps.map(letter => letter.toLowerCase())
+    return lower.concat(caps);
+  })();
+
+  function wordScore(word) {
+    let score = 0;
+    for (let j = 0; j < word.length; j++) {
+      let char = word[j];
+      score += (key.indexOf(char) + 1);
+    }
+    return score;
+  }
+
+
+  console.log(wordScore('A'))
 for ( let i = 0; i < puzzleInputArr.length; i++) {
     let stringLength: number = puzzleInputArr[i].length
     let left: any = puzzleInputArr[i].slice(0, stringLength/2)
     let right: any = puzzleInputArr[i].slice(stringLength/2, stringLength)
     //console.log('left :>> ', left);
     //console.log('right :>> ', right);
-     console.log(anythingInCommon(left, right))
+     console.log(charInCommon(left, right))
 }
 
 // console.log(puzzleInputArr);
