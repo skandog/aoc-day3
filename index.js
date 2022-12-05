51894a2b50ed5706ffd5f164252ab2d4e5195f9d
skandog <muumrar@gmail.com>
2022-12-05 11:42:55 +0000

    separating items into left and right using for loop

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var puzzleInput = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
 var puzzleInputArr = puzzleInput.split('\n');
-// for ( let i = 0; i < )
-console.log(puzzleInputArr);
+for (var i = 0; i < puzzleInputArr.length; i++) {
+    var stringLength = puzzleInputArr[i].length;
+    var left = puzzleInputArr[i].slice(0, stringLength / 2);
+    var right = puzzleInputArr[i].slice(stringLength / 2, stringLength);
+    console.log('left :>> ', left);
+    console.log('right :>> ', right);
+}
+// console.log(puzzleInputArr);
--- index.ts
+++ index.ts
@@ -9,6 +9,12 @@ CrZsJsPPZsGzwwsLwLmpwMDw`
 
 let puzzleInputArr: any = puzzleInput.split('\n')
 
-// for ( let i = 0; i < )
+for ( let i = 0; i < puzzleInputArr.length; i++) {
+    let stringLength: number = puzzleInputArr[i].length
+    let left: any = puzzleInputArr[i].slice(0, stringLength/2)
+    let right: any = puzzleInputArr[i].slice(stringLength/2, stringLength)
+    console.log('left :>> ', left);
+    console.log('right :>> ', right);
+}
 
-console.log(puzzleInputArr);
+// console.log(puzzleInputArr);
