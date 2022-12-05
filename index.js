5f90b3ec7d7c9469d94dfd4bcea4e304d4c2d451
skandog <muumrar@gmail.com>
2022-12-05 11:37:05 +0000

    inital commit - setup dummy input and split into array

---
+++ index.js
@@ -0,0 +1,4 @@
+var puzzleInput = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
+var puzzleInputArr = puzzleInput.split('\n');
+// for ( let i = 0; i < )
+console.log(puzzleInputArr);
---
+++ index.ts
@@ -0,0 +1,14 @@
+
+
+const puzzleInput: any = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`
+
+let puzzleInputArr: any = puzzleInput.split('\n')
+
+// for ( let i = 0; i < )
+
+console.log(puzzleInputArr);
