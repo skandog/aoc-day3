eb4c590adda4a875ec858d8820f9a77222cb152e
skandog <muumrar@gmail.com>
2022-12-05 11:55:34 +0000

    added function to find common char

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var puzzleInput = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
 var puzzleInputArr = puzzleInput.split('\n');
+var dupes = [];
+function anythingInCommon(a, b) {
+    // if( b.length < a.length )
+    //     return anythingInCommon(b, a)
+    for (var i = 0, len = a.length; i < len; i++)
+        if (b.indexOf(a[i]) != -1)
+            return (a[i]);
+    return false;
+}
 for (var i = 0; i < puzzleInputArr.length; i++) {
     var stringLength = puzzleInputArr[i].length;
     var left = puzzleInputArr[i].slice(0, stringLength / 2);
     var right = puzzleInputArr[i].slice(stringLength / 2, stringLength);
-    console.log('left :>> ', left);
-    console.log('right :>> ', right);
+    //console.log('left :>> ', left);
+    //console.log('right :>> ', right);
+    console.log(anythingInCommon(left, right));
 }
 // console.log(puzzleInputArr);
--- index.ts
+++ index.ts
@@ -9,12 +9,26 @@ CrZsJsPPZsGzwwsLwLmpwMDw`
 
 let puzzleInputArr: any = puzzleInput.split('\n')
 
+let dupes: string[] = [];
+
+function anythingInCommon(a, b){
+    // if( b.length < a.length )
+    //     return anythingInCommon(b, a)
+
+    for( var i = 0, len = a.length; i < len; i++ ) 
+        if(b.indexOf(a[i]) != -1)
+            return (a[i]);
+  
+    return false
+}
+
 for ( let i = 0; i < puzzleInputArr.length; i++) {
     let stringLength: number = puzzleInputArr[i].length
     let left: any = puzzleInputArr[i].slice(0, stringLength/2)
     let right: any = puzzleInputArr[i].slice(stringLength/2, stringLength)
-    console.log('left :>> ', left);
-    console.log('right :>> ', right);
+    //console.log('left :>> ', left);
+    //console.log('right :>> ', right);
+     console.log(anythingInCommon(left, right))
 }
 
 // console.log(puzzleInputArr);
